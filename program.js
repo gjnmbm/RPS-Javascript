42b0d9d85ebb452fb0caea36ed8bf30c70c58f1d
gjnmbm <gjnmbm@gmail.com>
2019-09-04 20:51:27 -0700

    Change if else statements to switch cases

--- program.js
+++ program.js
@@ -11,25 +11,45 @@ function setOpponentMove() {
 	opponentMove = possibleMoves[Math.floor(Math.random() * 3)];
 }
 function checkWin() {
-	if (move === 'rock' && opponentMove === 'scissors') {
-		document.getElementById("result").textContent = "Result: You win!";
-	}
-	else if (move === 'scissors' && opponentMove === 'paper') {
-		document.getElementById("result").textContent = "Result: You win!";
-	}
-	else if (move === 'paper' && opponentMove === 'rock') {
-		document.getElementById("result").textContent = "Result: You win!";
-	}
-	else if (move === 'rock' && opponentMove === 'paper') {
-		document.getElementById("result").textContent = "Result: You lose!";
-	}
-	else if (move === 'paper' && opponentMove === 'scissors') {
-		document.getElementById("result").textContent = "Result: You lose!";
-	}
-	else if (move === 'scissors' && opponentMove === 'rock') {
-		document.getElementById("result").textContent = "Result: You lose!";
-	}
-	else {
-		document.getElementById("result").textContent = "Result: It's a draw.";
-	}
+	switch (move) {
+    case 'rock':
+    switch (opponentMove) {
+      case 'paper':
+        document.getElementById("result").textContent = "Result: You lose!";
+        break;
+      case 'scissors':
+        document.getElementById("result").textContent = "Result: You win!";
+        break;
+	  default:
+        document.getElementById("result").textContent = "Result: It's a draw!";
+        break;
+    }
+    break;
+    case 'paper':
+    switch (opponentMove) {
+      case 'rock':
+        document.getElementById("result").textContent = "Result: You win!";
+        break;
+      case 'scissors':
+        document.getElementById("result").textContent = "Result: You lose!";
+        break;
+	  default:
+        document.getElementById("result").textContent = "Result: It's a draw!";
+        break;
+    }
+    break;
+    case 'scissors':
+    switch (opponentMove) {
+      case 'rock':
+        document.getElementById("result").textContent = "Result: You lose!";
+        break;
+      case 'paper':
+        document.getElementById("result").textContent = "Result: You win!";
+        break;
+      default:
+        document.getElementById("result").textContent = "Result: It's a draw!";
+        break;
+    }
+	break;
+  }
 }
