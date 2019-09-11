5a277e8f2117d1e745f0ca9df7458a4e5b1a5211
gjnmbm <gjnmbm@gmail.com>
2019-09-10 23:41:47 -0700

    Finalize gameplay and opponent move images

--- program.js
+++ program.js
@@ -5,20 +5,11 @@ let possibleMoves = ['rock', 'paper', 'scissors'];
 let images = ['images/rock.png', 'images/Paper.png', 'images/Scissors.png'];
 function setMove(decision) {
 	move = decision;
-	setOpponentMove();
+  setOpponentMove();
 	checkWin();
 }
 function setOpponentMove() {
   opponentMove = possibleMoves[Math.floor(Math.random() * 3)];
-  if (opponentMove = 'rock') {
-    document.getElementById('opponent').src = images[0];
-  }
-  else if (opponentMove = 'paper') {
-    document.getElementById('opponent').src = images[1];
-  }
-  else {
-    document.getElementById('opponent').src = images[2];
-  }
 }
 
 function checkWin() {
@@ -26,12 +17,15 @@ function checkWin() {
     case 'rock':
     switch (opponentMove) {
       case 'paper':
+        document.getElementById('opponent').src = images[1];
         document.getElementById("result").textContent = "Result: You lose!";
         break;
       case 'scissors':
+          document.getElementById('opponent').src = images[2];
         document.getElementById("result").textContent = "Result: You win!";
         break;
 	  default:
+        document.getElementById('opponent').src = images[0];
         document.getElementById("result").textContent = "Result: It's a draw!";
         break;
     }
@@ -39,12 +33,15 @@ function checkWin() {
     case 'paper':
     switch (opponentMove) {
       case 'rock':
+        document.getElementById('opponent').src = images[0];
         document.getElementById("result").textContent = "Result: You win!";
         break;
       case 'scissors':
+        document.getElementById('opponent').src = images[2];
         document.getElementById("result").textContent = "Result: You lose!";
         break;
 	  default:
+        document.getElementById('opponent').src = images[1];
         document.getElementById("result").textContent = "Result: It's a draw!";
         break;
     }
@@ -52,12 +49,15 @@ function checkWin() {
     case 'scissors':
     switch (opponentMove) {
       case 'rock':
+        document.getElementById('opponent').src = images[0];
         document.getElementById("result").textContent = "Result: You lose!";
         break;
       case 'paper':
+        document.getElementById('opponent').src = images[1];
         document.getElementById("result").textContent = "Result: You win!";
         break;
       default:
+        document.getElementById('opponent').src = images[2];
         document.getElementById("result").textContent = "Result: It's a draw!";
         break;
     }
